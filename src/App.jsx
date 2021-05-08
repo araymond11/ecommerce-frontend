a99de8406759a9e09ad8bffe35f4a5fae8c066bb
araymond11 <46835505+araymond11@users.noreply.github.com>
2021-05-08 11:34:02 -0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Products from './components/Products'
+import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+        <Route exact path='/'>
+          <Homepage/>
+        </Route>
+      <Switch>
+        <Route path='/products'>
+          <Products/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
